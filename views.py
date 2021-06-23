904568065a864627177975d3a38d3c91fa1e76ef
Farheen <fariparkar@gmail.com>
2021-06-23 13:21:05 +0530

    Add files via upload
    all the views are updated for Page Counter display

---
+++ views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from swk.HelloAnalytics import print_response, initialize_analyticsreporting, get_report
+
+# Create your views here.
+
+def Map(request):
+   analytics = initialize_analyticsreporting()
+   response = get_report(analytics)
+   recd_response = print_response(response)
+   context = {
+      'Visitor_count': recd_response
+   }
+   return render(request,"map/map.html",context)
